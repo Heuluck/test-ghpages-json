82660025f3c3ab03568bedf83333a052a385dc00
Heuluck Lu <sonicth@foxmail.com>
2025-02-17 13:55:48 +0800

    feat: delopy json only

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/test-ghpages-json/',
+    base: './',
     plugins: [react(), tailwindcss()],
 });
